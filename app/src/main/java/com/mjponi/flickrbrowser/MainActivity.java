e14aa82da6f0d7fb8146ada0d7b944fc69532a5a
markponi <markjponiatowski@gmail.com>
2020-07-03 17:06:00 -0400

    added new GetFlickrJsonData and GetRawData

--- app/src/main/java/com/mjponi/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/mjponi/flickrbrowser/MainActivity.java
@@ -10,7 +10,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable {
     private static final String TAG = "MainActivity";
 
     @Override
@@ -21,7 +23,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+//        GetRawData getRawData = new GetRawData(this);
+//        getRawData.execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop,sdk&tagmode=any&format=json&nojsoncallback=1");
+
         Log.d(TAG, "onCreate: ends");
+    }
+    
+    @Override
+    protected void onResume(){
+        Log.d(TAG, "onResume: starts");
+        super.onResume();
+
+        GetFlickrJsonData getFlickrJsonData = new GetFlickrJsonData(this, "https://www.flickr.com/services/feeds/photos_public.gne", "en-us", true);
+        getFlickrJsonData.executeOnSameThread("android, nougat");
+        Log.d(TAG, "onResume: ends");
     }
 
     @Override
@@ -46,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onOptionsItemSelected() returned: returned");
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDataAvailable(List<Photo> data, DownloadStatus status) {
+        if(status == DownloadStatus.OK){
+            Log.d(TAG, "onDataAvailable: data is " + data);
+        } else {
+            Log.e(TAG, "onDataAvailable: failed with status " + status);
+        }
     }
 }
