fecac1ddb4147d6fb62c90e9973afb1b8fb7e851
markponi <markjponiatowski@gmail.com>
2020-07-13 22:09:10 -0400

    Added in Recycler View on click listeners

--- app/src/main/java/com/mjponi/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/mjponi/flickrbrowser/MainActivity.java
@@ -11,11 +11,15 @@ import android.util.Log;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable {
+public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable,
+        RecyclerItemClickListener.OnRecyclerClickListener
+{
     private static final String TAG = "MainActivity";
     private FlickrRecyclerViewAdapter mFlickrRecyclerViewAdapter;
 
@@ -29,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView, this));
 
         mFlickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(this, new ArrayList<Photo>());
         recyclerView.setAdapter(mFlickrRecyclerViewAdapter);
@@ -84,5 +90,18 @@ public class MainActivity extends AppCompatActivity implements GetFlickrJsonData
         }
 
         Log.d(TAG, "onDataAvailable: ends");
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.d(TAG, "onItemClick: starts");
+        Toast.makeText(MainActivity.this, "Normal tap at position " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemLongClick(View view, int position) {
+        Log.d(TAG, "onItemLongClick: starts");
+        Toast.makeText(MainActivity.this, "Long tap at position " + position, Toast.LENGTH_SHORT).show();
+
     }
 }
